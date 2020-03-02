c118a15f1c8acad53560df8b582bb5ba2cbc114b
Nikesh <Nikesh.diyali.2021@student.lhschools.org>
2020-03-02 13:54:59 -0500

    IntelliJ Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("+--------------------+");
+        System.out.println("|    Gas Station     |");
+        System.out.println("|       Kroger       |");
+        System.out.println("|    Sale Receipt    |");
+        System.out.println("|                    |");
+        System.out.println("| Kroger             |");
+        System.out.println("| 7423 Skerrydoon Ln.|");
+        System.out.println("| Blacklick,Oh,43004 |");
+        System.out.println("|                    |");
+        System.out.println("| Date       Time    |");
+        System.out.println("| 01/16/2020 2:15P   |");
+        System.out.println("|                    |");
+        System.out.println("| Invoice# 10101010  |");
+        System.out.println("| Auth#              |");
+        System.out.println("|      CreditCard    |");
+        System.out.println("|   Account Number   |");
+        System.out.println("| ************13     |");
+        System.out.println("|                    |");
+        System.out.println("| Pump Product $/G   |");
+        System.out.println("|  08   UNLD  $2.80  |");
+        System.out.println("|                    |");
+        System.out.println("| Gallons Fuel Total |");
+        System.out.println("|  12.50     $ 35.00 |");
+        System.out.println("|                    |");
+        System.out.println("|     Thank You!!    |");
+        System.out.println("|    Gas Station     |");
+        System.out.println("|       Kroger       |");
+        System.out.println("+--------------------+");
+
+    }
+}
